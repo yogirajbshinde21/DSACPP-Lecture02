701035cc4caf076cbc8898139e35300c40491a96
Yogiraj Shinde <yogirajbshinde21@gmail.com>
2023-07-13 12:50:32 +0530

    first commit

---
+++ tut4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){  
+    int a=6;
+    int b = 7;
+    float pi = 3.14;
+    char ch = 'a';
+    cout<<"This is tutorial 4. \n Here the value of a is "<<a<<" Also the value of b is \n "<<b<<" The value of pi is"<<pi;
+    cout<<"Character is:"<<ch;
+    return 0;
+}
