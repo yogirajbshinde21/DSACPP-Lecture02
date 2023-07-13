3f2c9f80b1017c746bf6991744943f81a447d7c9
Yogiraj Shinde <yogirajbshinde21@gmail.com>
2023-07-13 12:39:41 +0530

    first commit

---
+++ tut3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+//"using namespace std;" --> (if you don't want to write std::cout)
+int main(){
+
+    int sum = 5;
+    std::cout<<"Hello World"<<sum;
+// 'cout' is used as printf in c++
+    return 0;
+}
